ca3d998658525e6849ffc80f0615714b8884d3ef
Hunter Lanier <68834499+Hunter-Lanier@users.noreply.github.com>
2024-06-17 11:30:44 -0500

    Added title, Added Error Handling, Added Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,59 @@
 <template>
-  <!-- Container -->
   <div class="Card h-full w-full bg-slate-50 flex justify-center items-center flex-col text-sm gap-10">
-    <!-- Type Selection -->
-    <div class=" flex w-screen text-lg justify-center">
-      <button @click="handleClick('length')"
-        :class="[[this.selectedType == 'length' ? ['bg-blue-500', 'text-blue-50'] : ''], ' border-2 border-blue-500 bg-blue-50 p-1 rounded-l-md']">Length</button>
-      <button @click="handleClick('temperature')"
-        :class="[[this.selectedType == 'temperature' ? ['bg-blue-500', 'text-blue-50'] : ''], ' border-2 border-blue-500 bg-blue-50 p-1 ']">Temperature</button>
-      <button @click="handleClick('area')"
-        :class="[[this.selectedType == 'area' ? ['bg-blue-500', 'text-blue-50'] : ''], ' border-2 border-blue-500 bg-blue-50 p-1']">Area</button>
-      <button @click="handleClick('volume')"
-        :class="[[this.selectedType == 'volume' ? ['bg-blue-500', 'text-blue-50'] : ''], ' border-2 border-blue-500 bg-blue-50 p-1']">Volume</button>
-      <button @click="handleClick('weight')"
-        :class="[[this.selectedType == 'weight' ? ['bg-blue-500', 'text-blue-50'] : ''], ' border-2 border-blue-500 bg-blue-50 p-1 rounded-r-md']">Weight</button>
+    <h1 class=" text-3xl  font-bold">Unit Converter</h1>
+    <div class="flex w-screen text-lg justify-center">
+      <button @click="handleClick('length')" :class="getButtonClass('length')">Length</button>
+      <button @click="handleClick('temperature')" :class="getButtonClass('temperature')">Temperature</button>
+      <button @click="handleClick('area')" :class="getButtonClass('area')">Area</button>
+      <button @click="handleClick('volume')" :class="getButtonClass('volume')">Volume</button>
+      <button @click="handleClick('weight')" :class="getButtonClass('weight')">Weight</button>
     </div>
 
     <div class="flex flex-row h-1/4 w-full justify-around items-center">
-      <!-- Input Form -->
-      <div class=" flex flex-col gap-4 justify-around ">
+      <div class="flex flex-col gap-4 justify-around">
         <input list="units-datalist" v-model="inputType" placeholder="From..."
-          class="rounded-md outline outline-2 outline-blue-500">
+          :class="errorMessages ? 'border-2 border-red-500' : 'border-2 border-blue-500'">
         <input v-model="inputValue" type="number" placeholder="0" class="rounded-md outline outline-2 outline-blue-500">
       </div>
       <div
-        :class="['flex flex-col w-auto h-max p-1 justify-center items-center rounded-md transition-all duration-300 active:bg-blue-500 active:text-blue-50 cursor-pointer']"
+        class="flex flex-col w-auto h-max p-1 justify-center items-center rounded-md transition-all duration-300 active:bg-blue-500 active:text-blue-50 cursor-pointer"
         @click="handleReverse()">
-        <!-- reverse icon-->
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3">
-          </path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
         </svg>
-        <!-- Mirror of prior Svg -->
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18">
-          </path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
         </svg>
-
-
       </div>
-      <div class=" flex flex-col gap-4 justify-around ">
+      <div class="flex flex-col gap-4 justify-around">
         <input list="units-datalist" v-model="resultType" placeholder="To..."
-          class="rounded-md outline outline-2 outline-blue-500">
-        <input v-model="this.convertedValue" type="number" placeholder="0"
+          :class="errorMessages ? 'border-2 border-red-500' : 'border-2 border-blue-500'">
+        <input v-model="convertedValue" type="number" placeholder="0"
           class="rounded-md outline outline-2 outline-blue-500">
       </div>
-      <!-- Datalist for units -->
       <datalist id="units-datalist">
-        <!-- Iterate over unitNames to display unit names -->
         <option v-for="unitName in unitNames" :key="unitName" :value="unitName">{{ unitName }}</option>
       </datalist>
-
     </div>
-
-
   </div>
 </template>
+
 <script>
 import Conversion from '@/assets/Conversion'
+import { errorMessages } from 'vue/compiler-sfc';
+
 export default {
   name: 'CardFormComponent',
   data() {
     return {
-      typeIdentifier: [
-        'length',
-        'temperature',
-        'area',
-        'volume',
-        'weight'
-      ],
+      typeIdentifier: ['length', 'temperature', 'area', 'volume', 'weight'],
       inputValue: null,
       selectedType: null,
       unitNames: [],
-      inputType: null,
-      resultType: null,
+      inputType: '',
+      resultType: '',
       conversion: new Conversion(),
+      errorMessages: null,
     }
   },
   methods: {
@@ -89,23 +69,58 @@ export default {
       if (!this.inputType || !this.resultType) {
         return;
       }
-      const temp = this.inputType;
-      this.inputType = this.resultType;
-      this.resultType = temp;
-      return;
-
-    }
+      [this.inputType, this.resultType] = [this.resultType, this.inputType];
+    },
+    validateTypes() {
+      if (!this.isValidTypes) {
+        this.errorMessages = 'Invalid type';
+      } else {
+        this.errorMessages = null;
+      }
+    },
   },
   computed: {
     convertedValue() {
-      if (!this.inputValue || !this.inputType || !this.resultType && this.inputValue !== NaN) {
+      if (!this.inputValue || !this.inputType || !this.resultType || isNaN(this.inputValue)) {
         console.log('Please fill in all fields');
-        return;
+        return null;
       }
-      this.resultValue = this.conversion.convert(this.inputValue, this.inputType, this.resultType, this.selectedType)
-      console.log(this.resultValue)
-      return this.resultValue
-    }
+      const resultValue = this.conversion.convert(this.inputValue, this.inputType, this.resultType, this.selectedType);
+      console.log(resultValue);
+      return resultValue;
+    },
+    isValidTypes() {
+      // Check if both inputType and resultType are either valid or empty
+      const isValidInputType = !this.inputType || this.unitNames.includes(this.inputType);
+      const isValidResultType = !this.resultType || this.unitNames.includes(this.resultType);
+      return isValidInputType && isValidResultType;
+    },
+    getButtonClass() {
+      return (type) => {
+        return [
+          'border-2',
+          'border-blue-500',
+          'bg-blue-50',
+          'p-1',
+          this.selectedType === type ? ['bg-blue-500', 'text-blue-50'] : ''
+        ];
+      };
+    },
+
+  },
+  watch: {
+    inputType: {
+      handler() {
+        this.validateTypes();
+      },
+      immediate: true, // To run the validation immediately after component initialization
+    },
+    resultType: {
+      handler() {
+        this.validateTypes();
+      },
+      immediate: true,
+    },
   }
 }
 </script>
